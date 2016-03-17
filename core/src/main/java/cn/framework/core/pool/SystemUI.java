fa6306082cf7aa031cc0bef6358292d4b821bfbe
wenlai.zhou <wenlai_zhou@126.com>
2016-03-17 11:19:35 +0800

    bug fix

--- core/src/main/java/cn/framework/core/pool/SystemUI.java
+++ core/src/main/java/cn/framework/core/pool/SystemUI.java
@@ -9,6 +9,7 @@ package cn.framework.core.pool;
 
 import cn.framework.core.container.TomcatContainer;
 import cn.framework.core.log.LogProvider;
+import cn.framework.core.utils.Exceptions;
 import cn.framework.core.utils.Files;
 import cn.framework.core.utils.Projects;
 import cn.framework.core.utils.Strings;
@@ -33,21 +34,23 @@ public class SystemUI extends HttpServlet {
      * 模板
      */
     public static final String TEMPLATE = "<tr><th scope=\"row\">${id}</th><td>${name}</td><td>${value}</td></tr>";
-
     /**
      * 模板
      */
     public static final String TABLE_TOTAL = "<table class=\"table table-striped\"><thead><tr><th>#</th><th>系统项</th><th>值</th></tr></thead><tbody><content></content></tbody></table>";
-
     /**
      * 模板
      */
     public static final String SYSTEM_INFO_UI = Files.readResourceText("cn/framework/core/pool/system.html");
-
     /**
      * long
      */
     private static final long serialVersionUID = -592302644015329197L;
+
+    /**
+     * 是否使用sigar监控系统信息
+     */
+    private static volatile boolean NO_USE_SIGAR = false;
 
     /**
      * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
@@ -57,29 +60,44 @@ public class SystemUI extends HttpServlet {
         if (!TomcatContainer.basicAuth(req, resp)) {
             return;
         }
-        resp.setContentType("text/html;charset=UTF-8;pageEncoding=UTF-8");
-        PrintWriter outPrinter = resp.getWriter();
         try {
-            Sigar sigar = new Sigar();
-            sigar.enableLogging(true);
-            StringBuilder html = new StringBuilder();
-            html.append(Strings.format(TEMPLATE, newPair("id", 1), newPair("name", "cpu"), newPair("value", String.format("%.2f%%", (1 - sigar.getCpuPerc().getIdle()) * 100))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 2), newPair("name", "memory"), newPair("value", String.format("%.2f%%", sigar.getMem().getUsedPercent()))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 3), newPair("name", "disk"), newPair("value", String.format("%.2f%%", sigar.getFileSystemUsage(Projects.PROJECT_DIR).getUsePercent()))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 4), newPair("name", "tcp-listens"), newPair("value", sigar.getNetStat().getTcpListen())));
-            html.append(Strings.format(TEMPLATE, newPair("id", 5), newPair("name", "tcp-estabs"), newPair("value", sigar.getTcp().getCurrEstab())));
-            html.append(Strings.format(TEMPLATE, newPair("id", 6), newPair("name", "process-cpu"), newPair("value", String.format("%.2f%%", sigar.getProcCpu(sigar.getPid()).getPercent()))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 7), newPair("name", "process-mem"), newPair("value", String.format("%sMB", sigar.getProcMem(sigar.getPid()).getSize() / 1024 / 1024))));
-            sigar.close();
-            outPrinter.append(SYSTEM_INFO_UI.replace("${total}", TABLE_TOTAL).replace("<content></content>", html));
+            resp.setContentType("text/html;charset=UTF-8;pageEncoding=UTF-8");
+            PrintWriter outPrinter = resp.getWriter();
+            if (NO_USE_SIGAR) {
+                StringBuilder html = new StringBuilder();
+                html.append(Strings.format(TEMPLATE, newPair("id", 1), newPair("name", "cpu"), newPair("value", String.format("%.2f%%", ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage()))));
+                html.append(Strings.format(TEMPLATE, newPair("id", 2), newPair("name", "heap-memory"), newPair("value", String.format("%1sMB", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024 / 1024))));
+                html.append(Strings.format(TEMPLATE, newPair("id", 3), newPair("name", "loaded-class-count"), newPair("value", String.format("%1$s", ManagementFactory.getClassLoadingMXBean().getLoadedClassCount()))));
+                outPrinter.append(SYSTEM_INFO_UI.replace("${total}", TABLE_TOTAL).replace("<content></content>", html));
+            }
+            else {
+                try {
+                    Sigar sigar = new Sigar();
+                    sigar.enableLogging(true);
+                    StringBuilder html = new StringBuilder();
+                    html.append(Strings.format(TEMPLATE, newPair("id", 1), newPair("name", "cpu"), newPair("value", String.format("%.2f%%", (1 - sigar.getCpuPerc().getIdle()) * 100))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 2), newPair("name", "memory"), newPair("value", String.format("%.2f%%", sigar.getMem().getUsedPercent()))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 3), newPair("name", "disk"), newPair("value", String.format("%.2f%%", sigar.getFileSystemUsage(Projects.PROJECT_DIR).getUsePercent()))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 4), newPair("name", "tcp-listens"), newPair("value", sigar.getNetStat().getTcpListen())));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 5), newPair("name", "tcp-estabs"), newPair("value", sigar.getTcp().getCurrEstab())));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 6), newPair("name", "process-cpu"), newPair("value", String.format("%.2f%%", sigar.getProcCpu(sigar.getPid()).getPercent()))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 7), newPair("name", "process-mem"), newPair("value", String.format("%sMB", sigar.getProcMem(sigar.getPid()).getSize() / 1024 / 1024))));
+                    sigar.close();
+                    outPrinter.append(SYSTEM_INFO_UI.replace("${total}", TABLE_TOTAL).replace("<content></content>", html));
+                }
+                catch (Throwable x) {
+                    NO_USE_SIGAR = true;
+                    LogProvider.getFrameworkErrorLogger().error(x.getMessage(), x);
+                    StringBuilder html = new StringBuilder();
+                    html.append(Strings.format(TEMPLATE, newPair("id", 1), newPair("name", "cpu"), newPair("value", String.format("%.2f%%", ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage()))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 2), newPair("name", "heap-memory"), newPair("value", String.format("%1sMB", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024 / 1024))));
+                    html.append(Strings.format(TEMPLATE, newPair("id", 3), newPair("name", "loaded-class-count"), newPair("value", String.format("%1$s", ManagementFactory.getClassLoadingMXBean().getLoadedClassCount()))));
+                    outPrinter.append(SYSTEM_INFO_UI.replace("${total}", TABLE_TOTAL).replace("<content></content>", html));
+                }
+            }
         }
-        catch (Throwable x) {
-            LogProvider.getFrameworkErrorLogger().error(x.getMessage(), x);
-            StringBuilder html = new StringBuilder();
-            html.append(Strings.format(TEMPLATE, newPair("id", 1), newPair("name", "cpu"), newPair("value", String.format("%.2f%%", ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage()))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 2), newPair("name", "heap-memory"), newPair("value", String.format("%1sMB", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024 / 1024))));
-            html.append(Strings.format(TEMPLATE, newPair("id", 3), newPair("name", "loaded-class-count"), newPair("value", String.format("%1$s", ManagementFactory.getClassLoadingMXBean().getLoadedClassCount()))));
-            outPrinter.append(SYSTEM_INFO_UI.replace("${total}", TABLE_TOTAL).replace("<content></content>", html));
+        catch (Exception x) {
+            Exceptions.processException(x);
         }
     }
 }
